d9828f4a5655eaa03cfcdab7d90c57a90371477b
kz <parumu4job@gmail.com>
2020-05-16 19:48:00 +0900

    add custom debug msg to SerErr

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use byteorder::{ByteOrder, BigEndian};
+use std::fmt;
 
 pub enum Item {
   List(usize, Vec<Item>),
@@ -14,12 +15,22 @@ struct ByteStream<'a> {
 
 pub type StreamIndex = usize;
 
-#[derive(Debug)]
 pub enum SerErr {
   NoLengthHeader(StreamIndex),
   NoData(usize, StreamIndex),
   NoLengthSize(usize, StreamIndex),
   RedundantData(StreamIndex),
+}
+
+impl fmt::Debug for SerErr {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    match *self {
+      SerErr::NoLengthHeader(idx) => write!(f, format!("No length header at {}", idx)),
+      SerErr::NoData(size, idx) => write!(f, format!("No data of size {} at {}", size, idx)),
+      SerErr::NoLengthSize(size, idx) => write!(f, format!("No length size of {} at {}", size, idx)),
+      SerErr::RedundantData(idx) => write!(f, format!("Redundant data found at {}", idx)),
+    }
+  }
 }
 
 enum TakeResult<'a> {
